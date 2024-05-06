caf8858a97b192e0b81b1599856a3383489f098e
MsSuraj001 <158797669+MsSuraj001@users.noreply.github.com>
2024-05-06 11:15:43 +0530

    This is datatype in python

---
+++ Datatype.py
@@ -0,0 +1,33 @@
+#Numarical data type
+# x = 3
+# print(type(x))
+#
+# price = 3.55
+# print(type(price))
+# print(id(price))   #this is the print of id for price
+
+#complex data type
+
+# com_num = 3e6
+# print(com_num)
+#
+# z = 4 + 5j  #what is this go and head this point
+# print(z)
+
+
+# Python world some inbuild functions
+
+print(abs(-5))  # this function is always return the absalute value
+
+print(pow(2,5))  # this function is return the pow of number 32
+
+print(round(3.75)) # this function is return the nearest value 4
+
+print(round(3.5)) # this function is return the nearest value 3
+
+
+nums = [1,2,3,5,3,6,8,9]
+
+print(min(nums))  #1
+print(max(nums))  #9
+print(sum(nums))   #37
